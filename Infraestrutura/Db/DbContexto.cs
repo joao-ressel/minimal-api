986b73585a19b1ba9c3c66dc604dbeddbc143a06
joao-ressel <joao.resselti@gmail.com>
2024-08-29 14:18:17 -0500

    feat: configurando o projeto com entity e criando a tabela administradores

--- Infraestrutura/Db/DbContexto.cs
+++ Infraestrutura/Db/DbContexto.cs
@@ -1,8 +1,29 @@
+using Microsoft.EntityFrameworkCore;
+using MinimalApi.Dominio.Entidades;
 
 namespace MinimalApi.Infraestrutura.Db
 {
-    public class DbContexto
+    public class DbContexto : DbContext
     {
-        
+        private readonly IConfiguration _configuracaoAppSettings;
+
+        public DbContexto(IConfiguration configuracaoAppSettings)
+        {
+            _configuracaoAppSettings = configuracaoAppSettings;
+        }
+
+        public DbSet<Adminstrador> Administradores { get; set; } = default!;
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+                var stringConexao = _configuracaoAppSettings.GetConnectionString("mysql");
+                if (!string.IsNullOrEmpty(stringConexao))
+                {
+                    optionsBuilder.UseMySql(stringConexao, ServerVersion.AutoDetect(stringConexao));
+                }
+            }
+        }
     }
 }
--- Program.cs
+++ Program.cs
@@ -1,4 +1,15 @@
+using Microsoft.EntityFrameworkCore;
+using MinimalApi.Infraestrutura.Db;
+
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddDbContext<DbContexto>(options => {
+    options.UseMySql(
+        builder.Configuration.GetConnectionString("mysql"),
+        ServerVersion.AutoDetect(builder.Configuration.GetConnectionString("mysql"))
+    );
+});
+
 var app = builder.Build();
 
 app.MapGet("/", () => "Hello World!");
