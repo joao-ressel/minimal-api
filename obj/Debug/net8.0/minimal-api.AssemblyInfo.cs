b7b8fd5dd3afd1487e1fa7aa662ff408002d1d54
joao-ressel <joao.resselti@gmail.com>
2024-10-23 10:39:17 -0500

    finalizando testes

--- obj/Debug/net8.0/minimal-api.AssemblyInfo.cs
+++ obj/Debug/net8.0/minimal-api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("minimal-api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9c088edf47131387d1aa1e4866cb79898c8e485b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b697730efcebf28f0798185c641626a0949cc7c2")]
 [assembly: System.Reflection.AssemblyProductAttribute("minimal-api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("minimal-api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
